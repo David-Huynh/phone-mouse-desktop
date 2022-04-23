67cc2ec0106057758879ea8ad4819468f82763b2
david-huynh <huynh.david.work@gmail.com>
2022-04-23 02:29:42 -0400

    removed unused code

--- src/main/java/com/dhuynh/App.java
+++ src/main/java/com/dhuynh/App.java
@@ -77,15 +77,6 @@ public class App extends Application {
         stage.show();
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
     public static void main(String[] args) {
         BlockingQueue<Integer[]> queue = (BlockingQueue<Integer[]>) SocketQueue.getInstance();
 
